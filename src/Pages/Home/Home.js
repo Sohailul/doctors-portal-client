83ad69c3f85db87ccc424cb227487f80ca4add90
sohailul <sohailulalam@gmail.com>
2022-05-10 23:14:04 +0600

    testimonials and contact section created

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -9,12 +10,17 @@ import Treatment from './Treatment';
 const Home = () => {
     return (
         <div>
+            <div className='max-w-7xl mx-auto px-10'>
             <Banner />
             <Info />
             <Services/>
             <Treatment/>
+            </div>
             <MakeAppointment/>
+            <div className='max-w-7xl mx-auto px-10'>
             <Testimonials/>
+            </div>
+            <Contact/>
         </div>
     );
 };
