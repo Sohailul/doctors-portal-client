269e826cfb91bd967df3c341c4ccba24ec725d61
sohailul <sohailulalam@gmail.com>
2022-05-10 13:41:32 +0600

    info and infoCard component updated

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,11 +5,21 @@ import marker from '../../assets/icons/marker.svg';
 import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
+    const openingHours = <>
+    <p>Lorem Ipsum is simply dummy text of the pri</p>
+    </>
+    const location = <>
+    <p>Brooklyn, NY 10036, United States</p>
+    </>
+
+    const contact = <>
+    <p>+000 123 456789</p>
+    </>
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={marker}></InfoCard>
-            <InfoCard img={phone}></InfoCard>
+            <InfoCard cardTitle="Opening hours" bgClass="bg-gradient-to-r from-secondary to-primary" info={openingHours} img={clock}></InfoCard>
+            <InfoCard cardTitle="Visit our location" bgClass="bg-accent" info={location} img={marker}></InfoCard>
+            <InfoCard cardTitle="Contact us now" bgClass="bg-gradient-to-r from-secondary to-primary" info={contact} img={phone}></InfoCard>
         </div>
     );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const InfoCard = ({img}) => {
+const InfoCard = ({img, cardTitle, bgClass, info}) => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl bg-primary">
+        <div className={`card lg:card-side bg-base-100 shadow-xl px-5 ${bgClass}`}>
             <figure><img src={img} alt="Album" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-body text-white">
+                <h2 className="card-title">{cardTitle}</h2>
+                <p>{info}</p>
             </div>
         </div>
     );
